39f1e8cd3341f91bb109436f1e17502c545cb6ac
Viktor Dorokhov <superdva@yandex.ru>
2024-02-07 18:13:50 +0300

    Setting up the environment

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-### Hexlet tests and linter status:
+# RSS агрегатор
+
+[![Actions Status](https://github.com/viktor-dorokhov/frontend-project-11/actions/workflows/code-check.yml/badge.svg)](https://github.com/viktor-dorokhov/frontend-project-11/actions)
 [![Actions Status](https://github.com/viktor-dorokhov/frontend-project-11/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/viktor-dorokhov/frontend-project-11/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/d2dee2ce319ef6b6c93d/maintainability)](https://codeclimate.com/github/viktor-dorokhov/frontend-project-46/maintainability)
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  entry: './src/index.js',
+  output: {
+    clean: true,
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+};
